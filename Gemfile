abc402d2dc71c43cc0fb7924e19b5540fc3f8ed8
phillipug <phillipmusiime@gmail.com>
2020-06-11 23:53:22 +0300

    added 'accept' action in users controller

--- Gemfile
+++ Gemfile
@@ -40,7 +40,6 @@ gem 'devise'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'hirb'
 end
 
@@ -50,6 +49,7 @@ end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
+  gem 'byebug'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'web-console', '>= 3.3.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @pending_friends = @user.pending_friends
+    # @my_friends = @user.my_friends
+    @friend_requests = @user.friend_requests
   end
 
   def invite
@@ -16,7 +19,21 @@ class UsersController < ApplicationController
   	if @friendship.save
   		flash[:notice] = "Request Sent"
   		redirect_to users_path
+  	else
+  		flash[:alert] = "Friend request already sent!"
+  		redirect_to users_path
   	end
 
   end
+
+  def accept
+  	@friendship = Friendship.find_by(friend_id: params[:id])
+  	# byebug
+  	friendship_params = params.require(:friendship).permit(:state)
+  	if @friendship.update(friendship_params)  	
+  		redirect_to users_path, notice: "Status Successfully Changed"
+  	end
+  end
+
+  
 end
