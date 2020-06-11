e048e1300d143d5a3f1d1fb5aa376b2f9292bdd8
phillipug <phillipmusiime@gmail.com>
2020-06-11 23:51:48 +0300

    added helper methods in user model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,8 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
 
+  validates :user_id, uniqueness: {scope: :friend_id}
+
   def self.add_friend(user_id, friend_id, state)
   	self.create(user_id: user_id, friend_id: friend_id, state: state)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,31 +10,32 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, class_name: :Friendship, foreign_key: :friend_id
+  has_many :friends, class_name: :Friendship, foreign_key: :friend_id
 
-  def friends
-  	friends_array = friendships.map {|friendship| friendship.friend if friendship.confirmed }
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+  def my_friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.state == "confirmed"}
+    friends_array + friends.map{|friendship| friendship.user if friendship.state == "confirmed"}
     friends_array.compact
   end
 
-    # Users who have yet to confirme friend requests
+  # Users who have yet to confirm friend requests
   def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friendships.map{|friendship| friendship.friend if friendship.state == "pending"}.compact
   end
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    friends.map{|friendship| friendship.user if friendship.state == "pending"}.compact
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship = friends.find{|friendship| friendship.user == user}
     friendship.confirmed = true
     friendship.save
   end
 
   def friend?(user)
-    friends.include?(user)
+    my_friends.include?(user)
   end
+
 end
