5edd28ac06df8c138403a69c8ce3ea55f776f722
phillipug <phillipmusiime@gmail.com>
2020-06-11 15:19:31 +0300

    added invite action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+
+  def invite
+  	@friendship = Friendship.add_friend(current_user.id, params[:friend_id], "pending")
+
+  	if @friendship.save
+  		flash[:notice] = "Request Sent"
+  		redirect_to users_path
+  	end
+
+  end
 end
