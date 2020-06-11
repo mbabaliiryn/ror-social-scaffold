3cbf850462f7a3400a2902b316cfda0488624123
phillipug <phillipmusiime@gmail.com>
2020-06-11 15:20:43 +0300

    Added 'add_friend' class method to Friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,8 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
+
+  def self.add_friend(user_id, friend_id, state)
+  	self.create(user_id: user_id, friend_id: friend_id, state: state)
+  end
 end
