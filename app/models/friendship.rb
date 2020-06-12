08545af2cfca90f674a3afd5705c9139d7848698
phillipug <phillipmusiime@gmail.com>
2020-06-12 16:23:51 +0300

    modified 'add friend' method for mutual frienship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,8 @@ class Friendship < ApplicationRecord
   validates :user_id, uniqueness: {scope: :friend_id}
 
   def self.add_friend(user_id, friend_id, state)
-  	self.create(user_id: user_id, friend_id: friend_id, state: state)
+  	user_friendship = self.create(user_id: user_id, friend_id: friend_id, state: state)
+  	friend_friendship = self.create(user_id: friend_id, friend_id: user_id, state: state)
+  	[user_friendship, friend_friendship]
   end
 end
