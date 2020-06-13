102e9cce72eec1a89e1ca6d1672ecec348c04e43
phillipug <phillipmusiime@gmail.com>
2020-06-13 23:58:24 +0300

    added model specs

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Friendship, type: :model do
+	describe 'Associations' do
+		it {should belong_to(:user)}
+		it {should belong_to(:friend).class_name('User')}
+	end
+
+	describe 'Class Method ::add_friend' do
+		let(:user_id) {28}
+		let(:friend_id) {14}
+		let(:state) {'pending'}
+		it 'should return an Array' do
+			expect(Friendship.add_friend(user_id, friend_id, state)).to be_an Array
+		end
+
+		it 'returned array should contain frienship objects' do
+			result = Friendship.add_friend(user_id, friend_id, state)
+			expect(result).to include(Friendship)
+
+		end
+	end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'Validations' do
+  	it {should validate_presence_of(:content)}
+  	it {should validate_length_of(:content).is_at_most(1000).with_message("1000 characters in post is the maximum allowed.")}
+  end
+
+  describe 'Associations' do  	
+  	it {should belong_to(:user)}
+  	it {should have_many(:likes).dependent(:destroy)}
+  	it {should have_many(:comments).dependent(:destroy)}
+  end
+
+  describe 'Scope' do
+  	it 'applies a :ordered_by_most_recent scope to posts by datetime created descending' do
+  		expect(Post.all.ordered_by_most_recent.to_sql).to eq Post.all.order(created_at: :desc).to_sql
+  	end
+  end
+end
