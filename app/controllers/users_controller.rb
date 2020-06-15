a2d36f989a225696168a322e77e829f426b91609
phillipug <phillipmusiime@gmail.com>
2020-06-15 13:12:35 +0300

    modified accept, reject and invite actions in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
     @pending_friends = @user.pending_friends
-    @my_friends = @user.my_friends
     @friend_requests = @user.friend_requests
+    @my_friends = current_user.my_friends
   end
 
   def invite
@@ -20,19 +20,19 @@ class UsersController < ApplicationController
   end
 
   def accept
-    @friendship1 = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
-    @friendship2 = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
+    @friendship = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
     friendship_params = params.require(:friendship).permit(:state)
-    @friendship1.update(friendship_params)
-    @friendship2.update(friendship_params)
-    redirect_to user_path(current_user.id), notice: 'Friend Successfully Accepted!'
+    if @friendship.update(friendship_params)
+      redirect_to user_path(current_user.id), notice: 'Friend Successfully Accepted!'
+    else
+      flash[:alert] = "An error Occurred!"
+      redirect_to users_path
+    end
   end
 
   def reject
-    @friendship1 = Friendship.find_by(friend_id: params[:id], user_id: current_user.id)
-    @friendship2 = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
-    @friendship1.destroy
-    @friendship2.destroy
+    @friendship = Friendship.find_by(friend_id: current_user.id, user_id: params[:id])
+    @friendship.destroy
     redirect_to user_path(current_user.id), alert: 'Removed!'
   end
 end
