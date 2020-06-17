dcf7459071096d7348e7effec63023cac86201c3
phillipug <phillipmusiime@gmail.com>
2020-06-17 09:56:44 +0300

    creates two records with different states on 'add-friend'

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,6 +4,7 @@ class Friendship < ApplicationRecord
 
   def self.add_friend(user_id, friend_id, state)
     user_friendship = create(user_id: user_id, friend_id: friend_id, state: state)
-    [user_friendship]
+    friend_friendship = create(user_id: friend_id, friend_id: user_id, state: "request-sent")
+  	[user_friendship, friend_friendship]
   end
 end
