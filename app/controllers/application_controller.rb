21fc5d2e544c07637a16ea6e6ed221e110fdbc7d
mbabali iryn <mbabaliiryn@gmail.com>
2020-10-06 16:35:23 +0300

    update sign form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :contact, :first_name, :last_name, :nationality, :user_type, :bio, :DOB) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_064042) do
+ActiveRecord::Schema.define(version: 2020_10_06_123236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 2020_06_10_064042) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "gravatar_url"
+    t.string "contact"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nationality"
+    t.string "DOB"
+    t.string "user_type"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
