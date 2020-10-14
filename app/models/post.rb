5b030935e90a42c882efb86207e030e134ef9924
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-14 17:41:52 +0300

    Fix delete post bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_one_attached :video
-  has_many :votes
+  has_many :votes, dependent: :destroy
   has_many :votants, through: :votes
 end
